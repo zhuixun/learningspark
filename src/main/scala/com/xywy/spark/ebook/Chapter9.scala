28e91dc3accd50df8080905d52e357adee5b12d7
zhuixun <xnbigdata@gmail.com>
2015-06-18 14:48:47 +0800

    TF-IDF

--- src/main/scala/com/xywy/spark/ebook/Chapter9.scala
+++ src/main/scala/com/xywy/spark/ebook/Chapter9.scala
@@ -1,7 +1,9 @@
 package com.xywy.spark.ebook
 
 import org.apache.spark.{SparkContext, SparkConf}
-
+import org.apache.spark.mllib.linalg.{ SparseVector => SV }
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.feature.IDF
 /**
  * Advanced Text Processing with Spark
  * Created by 49236_000 on 2015/6/16.
@@ -46,4 +48,71 @@ object Chapter9{
 
   //test
   println(nonWordSplit.distinct().sample(true,0.3,42).take(100).mkString(","))
+
+  //regex
+  val regex="""[^0-9]*""".r
+  val filterNumbers=nonWordSplit.filter(token=>
+  regex.pattern.matcher(token).matches()
+  )
+  println(filterNumbers.distinct().count())
+  println(filterNumbers.distinct.sample(true, 0.3, 42).take(100).mkString(","))
+
+  //Removing stop words
+  val tokenCounts = filterNumbers.map(t => (t, 1)).reduceByKey(_ + _)
+  val oreringDesc=Ordering.by[(String,Int),Int](_._2)
+  println(tokenCounts.top(20)(oreringDesc).mkString("\n"))
+
+  val stopWords=Set("the","a","an","of","or","in","for","by","on","but", "is", "not","with", "as", "was", "if","they", "are", "this", "and", "it", "have", "from", "at", "my","be", "that", "to")
+  val tokenCountsFilteredStopwords=tokenCounts.filter{ case
+    (k,v) => !stopWords.contains(k)
+  }
+//  println(tokenCountsFilteredStopwords.top(20)(oreringDesc).mkString("\n"))
+
+  val tokenCountsFilteredSize=tokenCountsFilteredStopwords.filter{
+    case(k,v) => k.size >= 2
+  }
+  println(tokenCountsFilteredSize.top(20)(oreringDesc).mkString("\n"))
+
+  //Excluding terms based on frequency
+  val oreringAsc=Ordering.by[(String,Int),Int](_._2)
+  println(tokenCountsFilteredSize.top(20)(oreringAsc).mkString("\n"))
+
+  val rareTokens=tokenCounts.filter{
+    case (k,v) => v<2
+  }.map{
+    case(k,v) =>k
+  }.collect.toSet
+
+  val tokenCountsFilteredAll=tokenCountsFilteredSize.filter{
+    case (k,v) => !rareTokens.contains(k)
+  }
+  println(tokenCountsFilteredAll.top(20)(oreringAsc).mkString("\n"))
+
+  println(tokenCountsFilteredAll.count)
+
+  def tokenize(line: String): Seq[String] = {
+    line.split("""\W+""")
+      .map(_.toLowerCase)
+      .filter(token => regex.pattern.matcher(token).matches)
+      .filterNot(token => stopWords.contains(token))
+      .filterNot(token => rareTokens.contains(token))
+      .filter(token => token.size >= 2)
+      .toSeq
+  }
+
+  val tokens=text.map(doc=>tokenize(doc))
+  println(tokens.first.take(20))
+
+  //Training a TF-IDF model
+  val dim=math.pow(2,18).toInt
+  val hashingTF=new HashingTF(dim)
+
+  val tf=hashingTF.transform(tokens)
+  tf.cache()
+
+  val v = tf.first.asInstanceOf[SV]
+  println(v.size)
+  println(v.values.size)
+  println(v.values.take(10).toSeq)
+  println(v.indices.take(10).toSeq)
 }
